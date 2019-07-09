a6dbc2815dc04c4092880c059087105814d1f029
Dennis Zelada <dennis.zelada@solarisbank.de>
2019-07-09 10:42:39 +0200

    Add Radium to use css styles like hover

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -49,7 +50,11 @@ class App extends Component {
       font: 'inherit',
       border: '1x solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -70,6 +75,12 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
+
     }
 
     const classes = [];
@@ -94,4 +105,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
