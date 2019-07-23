f52dcc1d2f4e8c25408d3cff51277a66ff5b3ad5
Dennis Zelada <denniszelada@gmail.com>
2019-07-23 22:14:09 +0200

    Stateless and stateful components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state= {
@@ -43,51 +44,21 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1x solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
 
     if (this.state.showPersons) {
-      persons = (
-        <div>
-        {this.state.persons.map((person, index) => {
-          return <Person
-            click = {() => this.deletePersonHandler(index)}
-            key = {person.id}
-            name = {person.name}
-            age = {person.age}
-            changed = {(event) => this.nameChangedHandler(event, person.id)}
-        />
-        })}
-        </div>
-      );
-
-      style.backgroundColor = 'red';
-    }
-
-    const classes = [];
-    if (this.state.persons.length <= 2) {
-      classes.push('red');
-    }
-    if (this.state.persons.length <=1) {
-      classes.push('bold');
+      persons = <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler} />
     }
 
     return (
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons
-        </button>
+        <Cockpit
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}/>
         {persons}
       </div>
     );
